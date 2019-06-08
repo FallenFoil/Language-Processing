49d5cc719f75ebaef0adbd8b860892d3ac190db0
CesarAugustoBorges <cesar.a.borges98@gmail.com>
2019-06-08 18:13:54 +0100

    Added basic code to structs.c

--- TP3/structs.c
+++ TP3/structs.c
@@ -15,6 +15,9 @@ typedef struct conceito
 	GSList *relations;
 } Conceito;
 
+Conceito* getConceito(char* concName){
+	return (Conceito *) g_hash_table_lookup(conceitos, concName);
+}
 
 Conceito* newConceito(char *name){
 	Conceito *conceito = (Conceito *) malloc(sizeof(Conceito));
@@ -25,4 +28,25 @@ Conceito* newConceito(char *name){
 
 void initConceitos(){
 	conceitos = g_hash_table_new(g_str_hash, g_str_equal);
+}
+
+void addConceito(char* conceitoKey){
+	Conceito *conceito = getConceito(conceitoKey);
+	if(!conceito){
+		conceito = newConceito(conceitoKey);
+		g_hash_table_insert(conceitos, conceito->name, conceito);
+	}
+}
+
+Relations* newRelations(char* relName){
+	Relations *relations = (Relations *) malloc(sizeof(Relations));
+	relations->name = strdup(relName);
+	relations->terms = NULL;
+	return relations;
+}
+
+void addRelationTo(char* relName, char* concName);
+
+GHashTable* getConceitos(){
+	return conceitos;
 }
