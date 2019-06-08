9c56ce3295159994f016bbe37c5b5f08de63c73f
CesarAugustoBorges <cesar.a.borges98@gmail.com>
2019-06-08 21:48:48 +0100

    pulling

--- TP3/structs.c
+++ TP3/structs.c
@@ -50,3 +50,31 @@ void addRelationTo(char* relName, char* concName);
 GHashTable* getConceitos(){
 	return conceitos;
 }
+
+
+void createHTML(){
+GHashTableIter iter;
+gpointer key, value;
+
+g_hash_table_iter_init (&iter, conceitos);
+while (g_hash_table_iter_next (&iter, &key, &value)){
+	char filePath[100] = "./html/conceitos/";
+	strcpy(filePath, key);
+	FILE * myfd = fopen(filePath, "w");
+
+    // do something with key and value
+  }
+}
+
+
+void BEGIN_HTML(FILE* file, title){
+	fprintf(file, "<!DOCTYPE html>\n<html lang=\"en\">\n\t<head>\n\t\t<title>");
+	fprintf(file, title); 
+	print "</title>\n\t\t<meta charset=\"UTF-8\">\n\t\t<meta name=\"description\" content=\"" > file
+	print title > file
+	print "\">\n\t\t<link href=\"https://fonts.googleapis.com/css?family=Montserrat:400,700\" rel=\"stylesheet\">\n\t</head>\n\t<body style=\"font-family: 'Montserrat', sans-serif;background-color: rgb(230, 230, 230);padding-right: 1%;padding-left: 1%\" >\n" > file
+}
+
+void END_HTML(FILE* file){
+	print "\n\t</body>\n</html>" > file
+}
